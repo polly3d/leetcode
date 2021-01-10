e2639334899c9bf06e7faa47e3cd2e3b892fa86b
Jim Wang <polly3d@126.com>
2021-01-10 10:01:58 +0800

    Add 389

--- 938. Range Sum of BST/938. Range Sum of BST.go
+++ 938. Range Sum of BST/938. Range Sum of BST.go
@@ -1,1 +1,29 @@
 package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+	sum := 0
+	inOrder(root, low, high, &sum)
+	return sum
+}
+
+func inOrder(root *TreeNode, low, hight int, sum *int) {
+	if root == nil {
+		return
+	}
+	inOrder(root.Left, low, hight, sum)
+	if low <= root.Val && root.Val <= hight {
+		*sum = *sum + root.Val
+	}
+	inOrder(root.Right, low, hight, sum)
+}
